876fe89016ce4630f18e084e297a4604f3e29714
Dylan Childs <dylan.childs@gmail.com>
2017-05-15 21:14:41 +0100

    temporary

--- init_prep.R
+++ init_prep.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(magrittr)
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
-## 
+## Read in / process the posterior samples
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 par_location <- "~/Dropbox/Shared Folders/Asymm_soay/results/Posteriors"
@@ -41,22 +41,78 @@ for (i in seq_along(file_names)) {
 }
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
-## 
+## Pull out samples from the posterior
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 processed_post <- 
-  bind_rows(processed_post) %>%
-  group_by(demog.var, parameter)
+  bind_rows(processed_post) %>% 
+  rename(p = parameter) %>%
+  mutate(
+    p = ifelse(p == "intercept",     "b_0_f",  p),
+    p = ifelse(p == "Intercept",     "b_0_f",  p),
+    p = ifelse(p == "sexM",          "b_0_m",  p),
+    p = ifelse(p == "z",             "b_z1_f", p),
+    p = ifelse(p == "z_sexM",        "b_z1_m", p),
+    p = ifelse(p == "age",           "b_a1_f", p),
+    p = ifelse(p == "sexM_age",      "b_a1_m", p),
+    p = ifelse(p == "IageE2",        "b_a2_f", p),
+    p = ifelse(p == "z_age",         "b_az_f", p),
+    p = ifelse(p == "sexM_IageE2",   "b_a2_m", p),
+    p = ifelse(p == "b_equivdens_1", "gamma",  p),
+    p = ifelse(p == "sex.lambM",     "b_0_m",  p),
+    p = ifelse(p == "offaretwins",   "b_0_tw", p),
+    p = ifelse(p == "z_birth.lamb",  "b_z1_lm", p)
+  ) %>%
+  group_by(demog.var, p)
 
-par_fix_post <- filter(processed_post, !grepl("r_2_", parameter))
-par_ran_post <- filter(processed_post,  grepl("r_2_", parameter))
+unique(processed_post$p)
+
+par_fix_post <- filter(processed_post, !grepl("r_2_", p))
+par_ran_post <- filter(processed_post,  grepl("r_2_", p))
+
+unique(processed_post$demog.var)
+
+## quick look at a specific posterior distribution
+processed_post %>% 
+  filter(
+    demog.var == "lamb.growth",
+    p == "b_a1_f"
+  ) %>%
+  ggplot(aes(x = value)) + 
+  geom_histogram() + 
+  geom_vline(xintercept = 0, col = "red")
+  
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+## 
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 par_fix <- 
   par_fix_post %>%
   select(-iteration, -chain) %>%
   summarise_if(is.numeric, mean)
+
+
+
+n_funs <- length(file_names)
+build_demog_funs <- vector(mode   = "list", length = n_funs)
+names(build_demog_funs) <- names(file_names)
+rm(n_funs)
+
+build_demog_funs$bw_lmb <- function(p, x1, x) {
   
-par_fix$parameter
+}
+  
+
+
+
+
+
+
+
+
+
+
 
 
 
